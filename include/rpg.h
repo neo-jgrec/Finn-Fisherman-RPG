0b4b679c439c310297a026a2bd1d1ebb4e38d8ff
alexis <alexis.boitel@epitech.eu>
2023-04-04 10:28:02 +0200

    feat(rpg.h) add csfml

---
+++ include/rpg.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** rpg
+** File description:
+** rpg.h
+*/
+
+#ifndef _RPG_H__
+    #define _RPG_H__
+    #include <SFML/Graphics.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <stdio.h>
+
+#endif /* !_RPG_H__ */
