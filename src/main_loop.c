1fb3f7175a1f00f583ba11d609db6176746354bf
alexis <alexis.boitel@epitech.eu>
2023-04-07 15:46:28 +0200

    feat(main_loop) add player draw

--- src/main_loop.c
+++ src/main_loop.c
@@ -18,8 +18,10 @@ static void analyse_events(win_t *win)
 int main_loop(win_t *win, rpg_t *rpg)
 {
     while (sfRenderWindow_isOpen(win->win)) {
+        manage_player(win, rpg->player, rpg);
         sfRenderWindow_clear(win->win, sfBlack);
         analyse_events(win);
+        sfRenderWindow_drawSprite(win->win, rpg->asset->player->sp, NULL);
         sfRenderWindow_display(win->win);
     }
     return 0;
