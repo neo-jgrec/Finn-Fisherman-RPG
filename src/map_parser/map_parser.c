3553718d9d55da95deb9376b35ff2bc583a7e4b2
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-04-12 11:35:17 +0200

    feat(main) base for map parsing

---
+++ src/map_parser/map_parser.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** rpg
+** File description:
+** map_parser
+*/
+
+#include "mars.h"
+
+int **map_parsing(char *map)
+{
+    int **map_int;
+
+    return (map_int);
+}
