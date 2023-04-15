c8bee73ab4f762b6f2fd72665e92f1600fbc6623
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-04-15 19:25:04 +0200

    fix(main_menu) add of the menu bg rescale

--- src/menu/buttons/functions_settings.c
+++ src/menu/buttons/functions_settings.c
@@ -19,12 +19,14 @@ void res_button(rpg_t *rpg)
         rpg->win->win = sfRenderWindow_create((sfVideoMode){1600, 900, 32},
             "THE RPG", sfClose | sfFullscreen, NULL);
         sfRenderWindow_setFramerateLimit(rpg->win->win, 60);
+        sfRectangleShape_setSize(rpg->menu->bg, (sfVector2f){1920, 1080});
         rpg->win->win_style = 0;
     } else {
         sfRenderWindow_close(rpg->win->win);
         rpg->win->win = sfRenderWindow_create((sfVideoMode){1600, 900, 32},
             "THE RPG", sfClose, NULL);
         sfRenderWindow_setFramerateLimit(rpg->win->win, 60);
+        sfRectangleShape_setSize(rpg->menu->bg, (sfVector2f){1600, 900});
         rpg->win->win_style = 1;
     }
 }
