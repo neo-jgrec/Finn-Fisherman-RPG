6cec98a627e60089e8366938aad3cc92ef3c3b57
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-04-16 23:33:53 +0200

    feat(main_menu) enhancement of the buttons

--- src/init/init_menu.c
+++ src/init/init_menu.c
@@ -25,11 +25,11 @@ static void init_buttons(menu_t *menu)
     char *buttons_names[] = {"PLAY", "SETTINGS", "QUIT"};
     void *buttons_functions[] = {play_button, settings_button, quit_button};
     sfVector2f buttons_pos[] = {{100, 300}, {100, 400}, {100, 500}};
-    sfVector2f buttons_size[] = {{200, 50}, {200, 50}, {200, 50}};
-    size_t nb_buttons = sizeof(buttons_names) / sizeof(char *);
+    sfVector2f buttons_size[] = {{TEXT_SIZE_LEN(buttons_names[0], 30), 30},
+    {TEXT_SIZE_LEN(buttons_names[1], 30), 30},
+    {TEXT_SIZE_LEN(buttons_names[2], 30), 30}};
 
-    for (size_t i = 0; i < nb_buttons; i++) {
-        button = malloc(sizeof(button_t));
+    for (size_t i = 0; i < 3; i++, button = malloc(sizeof(button_t))) {
         button->name = buttons_names[i];
         button->action = buttons_functions[i];
         button->pos = buttons_pos[i];
@@ -48,12 +48,12 @@ static void init_button_res_settings(menu_t *menu)
     char *buttons_names[] = {"1600x900", "1920x1080", "800x600"};
     void *buttons_functions[] = {res_1600_button, res_1920_button,
     res_800_button};
-    sfVector2f buttons_pos[] = {{1900 - 550, 70}, {1900 - 550, 130},
-    {1900 - 550, 190}};
-    sfVector2f buttons_size[] = {{200, 50}, {200, 50}, {200, 50}};
+    sfVector2f buttons_pos[] = {{1350, 70}, {1350, 130}, {1350, 190}};
+    sfVector2f buttons_size[] = {{TEXT_SIZE_LEN(buttons_names[0], 30),
+    30}, {TEXT_SIZE_LEN(buttons_names[1], 30), 30},
+    {TEXT_SIZE_LEN(buttons_names[2], 30), 30}};
 
-    for (size_t i = 0; i < 3; i++) {
-        button = malloc(sizeof(button_t));
+    for (size_t i = 0; i < 3; i++, button = malloc(sizeof(button_t))) {
         button->name = buttons_names[i];
         button->action = buttons_functions[i];
         button->pos = buttons_pos[i];
@@ -68,13 +68,13 @@ static void init_button_res_settings(menu_t *menu)
 
 static void init_button_settings(menu_t *menu)
 {
-    TAILQ_INIT(&menu->settings_buttons);
     button_t *button = malloc(sizeof(button_t));
     char *buttons_names[] = {"<- BACK", "SOUND", "MUSIC", "FULLSCREEN"};
     void *buttons_functions[] = {back_button, NULL, NULL, res_button};
     sfVector2f buttons_pos[] = {{10, 10}, {100, 300}, {100, 400}, {100, 500}};
-    sfVector2f buttons_size[] = {{200, 50}, {200, 50}, {200, 50}, {200, 50}};
-
+    sfVector2f buttons_size[] = {{TEXT_SIZE_LEN(buttons_names[0], 30), 30},
+    {TEXT_SIZE_LEN(buttons_names[1], 30), 30}, {TEXT_SIZE_LEN(buttons_names[2],
+    30), 30}, {TEXT_SIZE_LEN(buttons_names[3], 30), 30}};
     for (size_t i = 0; i < 4; i++, button = malloc(sizeof(button_t))) {
         button->name = buttons_names[i];
         button->action = buttons_functions[i];
@@ -110,10 +110,10 @@ static sfRenderStates *init_blur_renderstate(menu_t *menu)
 void init_menu(rpg_t *rpg)
 {
     menu_t *menu = malloc(sizeof(menu_t));
+
     menu->text = sfText_create();
     menu->font = sfFont_createFromFile("assets/Inter-Medium.ttf");
     menu->bg = sfRectangleShape_create();
-
     menu->render_states = init_blur_renderstate(menu);
     sfText_setString(menu->text, "THE RPG");
     sfText_setFont(menu->text, menu->font);
@@ -123,6 +123,7 @@ void init_menu(rpg_t *rpg)
     sfRectangleShape_setSize(menu->bg, (sfVector2f){1600, 900});
     sfRectangleShape_setFillColor(menu->bg, sfWhite);
     init_buttons(menu);
+    TAILQ_INIT(&menu->settings_buttons);
     init_button_settings(menu);
     menu->scene = MAIN_MENU;
     rpg->menu = menu;
--- src/menu/buttons/manager.c
+++ src/menu/buttons/manager.c
@@ -11,10 +11,8 @@
 static void button_state(sfRenderWindow *win, button_t *button, rpg_t *rpg)
 {
     sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(win);
-    sfVector2f button_pos = sfRectangleShape_getPosition(button->shape);
-    sfVector2f button_size = sfRectangleShape_getSize(button->shape);
 
-    if (is_rect_hover(mouse_pos, button_pos, button_size)) {
+    if (is_rect_hover(mouse_pos, button->pos, button->size)) {
         if (rpg->win->event.type == sfEvtMouseButtonPressed
             && rpg->win->event.mouseButton.button == sfMouseLeft
             && button->action != NULL) {
@@ -28,14 +26,10 @@ static void button_state(sfRenderWindow *win, button_t *button, rpg_t *rpg)
 
 static void change_button_style(button_t *button)
 {
-    sfColor white_with_alpha = {255, 255, 255, 100};
-
     switch (button->state) {
         case IDLE_BUTTON:
-            sfRectangleShape_setFillColor(button->shape, white_with_alpha);
             break;
         case HOVER_BUTTON:
-            sfRectangleShape_setFillColor(button->shape, sfWhite);
             break;
         case CLICKED_BUTTON:
             sfRectangleShape_setFillColor(button->shape, sfBlue);
@@ -52,11 +46,12 @@ void button_manager(win_t *win, rpg_t *rpg, struct buttons *button_list)
         button_state(win->win, button, rpg);
         change_button_style(button);
         sfText_setString(text, button->name);
-        sfText_setColor(text, sfBlack);
         sfText_setFont(text, rpg->menu->font);
-        sfText_setCharacterSize(text, 30);
+        sfText_setColor(text, sfWhite);
         sfText_setPosition(text, button->pos);
-        sfRenderWindow_drawRectangleShape(win->win, button->shape, NULL);
+        sfText_setCharacterSize(text, 30);
+        if (button->state == HOVER_BUTTON)
+            sfText_setCharacterSize(text, 35);
         sfRenderWindow_drawText(win->win, text, NULL);
     }
 }
