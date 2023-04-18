01c4a1887fa5a8755387066ff7d525ada930a2c9
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-04-18 13:47:40 +0200

    fix(main_menu) remove of a segv

--- src/init/init_saves.c
+++ src/init/init_saves.c
@@ -9,26 +9,34 @@
 #include <assert.h>
 #include "xml_parser.h"
 
+static void add_stats(save_menu_t *save, xml_parser_t *parser)
+{
+    save->level = get_value_by_tags(parser->root, ((char *[]){"LEVEL", NULL}));
+    save->name = get_value_by_tags(parser->root,
+    ((char *[]){"SAVENAME", NULL}));
+    save->is_write = (save->name != NULL);
+}
+
 void parse_saves(menu_t *menu)
 {
     char *saves[] = {"saves/save1.xml", "saves/save2.xml", "saves/save3.xml"};
-    size_t i = 0; menu->saves = malloc(sizeof(save_menu_t *) * 300);
+    size_t i = 0;
+    menu->saves = malloc(sizeof(save_menu_t *) * 300);
     sfVector2f *size[] = {&(sfVector2f){700, 185}, &(sfVector2f){700, 185},
     &(sfVector2f){700, 185}};
     sfVector2f *pos[] = {&(sfVector2f){1600 / 2 - 350, 200},
     &(sfVector2f){1600 / 2 - 350, 425}, &(sfVector2f){1600 / 2 - 350, 650}};
-    for (; i < 3; menu->saves[i] = malloc(sizeof(save_menu_t)), i++) {
+
+    for (; i < 3; i++) {
+        menu->saves[i] = malloc(sizeof(save_menu_t));
         menu->saves[i]->button = malloc(sizeof(button_t));
         menu->saves[i]->save_file = saves[i];
         menu->saves[i]->parser = init_parser(saves[i]);
         parse_xml(menu->saves[i]->parser);
-        menu->saves[i]->name = get_value_by_tags(menu->saves[i]->parser->root,
-        ((char *[]){"SAVENAME", NULL}));
-        menu->saves[i]->is_write = (menu->saves[i]->name != NULL);
+        add_stats(menu->saves[i], menu->saves[i]->parser);
         menu->saves[i]->button->shape = sfRectangleShape_create();
         sfRectangleShape_setSize(menu->saves[i]->button->shape, *size[i]);
         sfRectangleShape_setPosition(menu->saves[i]->button->shape, *pos[i]);
-        menu->saves[i]->button->name = menu->saves[i]->name;
     }
     menu->saves[i] = NULL;
 }
