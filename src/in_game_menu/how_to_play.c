00f7a3c81ea0191320bf17ab89d858d3bb0199f9
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-04-22 23:16:42 +0200

    style(game_menu) norm

--- src/in_game_menu/how_to_play.c
+++ src/in_game_menu/how_to_play.c
@@ -16,7 +16,6 @@ static void event_manager(win_t *win, rpg_t *rpg)
     sfRenderWindow_setView(win->win, sfRenderWindow_getDefaultView(win->win));
 }
 
-
 void draw_how_to(win_t *win, rpg_t *rpg)
 {
     event_manager(win, rpg);
@@ -29,4 +28,3 @@ void draw_how_to(win_t *win, rpg_t *rpg)
         sfRectangleShape_getSize(rpg->menu->how_to_play).y / 2});
     sfRenderWindow_drawRectangleShape(win->win, rpg->menu->how_to_play, NULL);
 }
-
