90524886fcd63d9e0c5079a4107b383b322c0af8
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-04-22 15:07:04 +0200

    feat(game_menu) item selection

--- src/in_game_menu/inventory.c
+++ src/in_game_menu/inventory.c
@@ -17,6 +17,17 @@ static void event_manager(rpg_t *rpg)
         sfRenderWindow_getDefaultView(rpg->win->win));
 }
 
+static void equip_inv(rpg_t *rpg, size_t i)
+{
+    for (size_t j = 0; (3 * 7) > j; j++) {
+        if (rpg->data->inventory[j] != NULL && rpg->data->inventory[j]->equiped
+        && rpg->data->inventory[j]->type == rpg->data->inventory[i]->type) {
+            rpg->data->inventory[j]->equiped = false;
+            rpg->data->inventory[i]->equiped = true;
+        }
+    }
+}
+
 static void handle_actions(rpg_t *rpg, sfVector2f pos, size_t i)
 {
     sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(rpg->win->win);
@@ -36,8 +47,7 @@ static void handle_actions(rpg_t *rpg, sfVector2f pos, size_t i)
         if (sfKeyboard_isKeyPressed(sfKeyR))
             rpg->data->inventory[i] = NULL;
         if (sfKeyboard_isKeyPressed(sfKeyE))
-            rpg->data->inventory[i]
-                ->equiped = !rpg->data->inventory[i]->equiped;
+            equip_inv(rpg, i);
     }
 }
 
@@ -47,7 +57,7 @@ void draw_inventory(rpg_t *rpg)
     sfVector2f pos;
 
     event_manager(rpg);
-    for (size_t i = 0; (3 * 8) > i; i++) {
+    for (size_t i = 0; (3 * 7) > i; i++) {
         if (!rpg->data->inventory[i]) continue;
         pos = (sfVector2f){((i % 8) * 64 + 300), ((i / 8) * 64 + 300)};
         texture_rect = (sfIntRect){(rpg->data->inventory[i]->sprite % 8) * 16,
