7d9638e572bd9daba5ef0208b0a746917afbfbe6
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-04-23 17:37:12 +0200

    feat(main) add of a help option

--- src/main.c
+++ src/main.c
@@ -9,9 +9,24 @@
 
 int verifier2000(void);
 
+static void help(void)
+{
+    my_printf("USAGE\t\t./my_rpg\n");
+    my_printf("DESCRIPTION\tThis is a RPG game made with CSFML.\n");
+    my_printf("\t\tFor more information, please read the documentation\
+:\n\t\t https://neo-19.gitbook.io/the-fisherman/\n");
+    my_printf("\nNOTE : If you do not have all the assets,\
+the game will not start at all.\n");
+}
+
 int main(int ac, UNUSED char **av)
 {
     srand(time(NULL));
+    if (ac == 2
+        && (!my_strcmp(av[1], "-h") || !my_strcmp(av[1], "--help"))) {
+        help();
+        return 0;
+    }
     if (ac != 1)
         return 0;
     if (verifier2000() == 84)
